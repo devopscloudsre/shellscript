347e1e6d668e71d163f58aa01ebe51b1b85c7d43
Rahul Chaudhuri <mailrahulsre@gmail.com>
2020-09-24 03:12:48 +0000

    var added

--- java_install.sh
+++ java_install.sh
@@ -2,7 +2,13 @@ yum install java-1.8* -y
 
 if [ $? -ne 0 ]; then
 	echo "Java  install is failed"
-	exit 0
+	exit 1
 else
-	echo "congratulations: Java is installed successfully"
+	echo "Congratulations: Java is installed successfully"
 fi
+
+cd /usr/lib/jvm/
+
+var=$(ls -ld java-1.8.0-openjdk-1.8.0* | awk '{print $9}')
+
+
