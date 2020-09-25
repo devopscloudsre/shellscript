a0083c16aef31b3065d6f1d2e23a9d58a76610f3
Rahul Chaudhuri <mailrahulsre@gmail.com>
2020-09-25 03:25:28 +0000

    final

--- java_install.sh
+++ java_install.sh
@@ -1,13 +1,26 @@
-yum install java-1.8* -y
+check () 
+{
 
-if [ $? -ne 0 ]; then
-	echo "Java  install is failed"
-	exit 1
-else
-	echo "Congratulations: Java is installed successfully"
+flag=$1
+step=$2
+
+if [ $flag -ne 0 ]; then
+        echo "step: $step is failed"
+        exit 1
+else   
+        echo ""
+        echo "Congratulations: step $step successful"
 fi
 
+}
+
+yum install java-1.8* -y
+
+check $? java-install 
+
 cd /usr/lib/jvm/
+
+check $? change-dir-java
 
 var=$(ls -ld java-1.8.0-openjdk-1.8.0* | awk '{print $9}')
 
@@ -15,8 +28,15 @@ var=$(ls -ld java-1.8.0-openjdk-1.8.0* | awk '{print $9}')
 
 cp $HOME/.bash_profile $HOME/.bash_profile-backup
 
+check $? backup_bash_profile
+
 echo "export JAVA_HOME=/usr/lib/jvm/$var" >> $HOME/.bash_profile
-echo "PATH=$JAVA_HOME/bin:$HOME/bin:$PATH" >> $HOME/.bash_profile
+echo 'export PATH=$JAVA_HOME/bin:$HOME/bin:$PATH' >> $HOME/.bash_profile
 
+source $HOME/.bash_profile
 
+check $? source_env_var
 
+echo 2 | alternatives --config java
+
+check $? java_config
